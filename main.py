c81a962be9069c9a24ae31e9b40b75119787b064
Gyro <gamingskullgamer@gmail.com>
2022-07-05 21:53:27 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from distutils.util import change_root
+from email import message
+from http import client
+from pydoc import cli
+from random import random, randrange
+import discord
+import json
+
+TOKEN = "OTkzOTQ2NjM0ODQzMDcwNTI0.GeDF9x.wS4FiffYx1mSTENwpNxnMpLpuRIjMPcgcByIks"
+f = open('walterDB.json', encoding='utf-8')
+db = json.load(f)
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+
+    current_message = user_message.lower()
+
+    if current_message == "amogus":
+        await message.channel.send(f"Hello {username}!")
+        return
+
+    wordsMSG = current_message.split()
+    keys = db["slavonijaSupremacyKeys"]
+
+    for word in wordsMSG:
+        if word in keys:
+            await message.channel.send(slavonijaSupremacy())
+            return
+
+
+def slavonijaSupremacy():
+    lines = db["slavonijaSupremacy"]
+    chosenMSG = randrange(0, len(lines))
+    return lines[chosenMSG]
+
+
+@client.event
+async def on_ready():
+    print("LOGIN SUCCESSFUL : {0.user}".format(client))
+
+client.run(TOKEN)
