65cba134ed5101bfb72a43fe54e7b0eddf0163b8
Gyro <gamingskullgamer@gmail.com>
2022-07-06 01:18:48 +0200

    Finished Integration with 15.ai voice syntheticism and intigratated it within discord using commands and voicechat feature

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from http import client
 from pydoc import cli
 from random import random, randrange
 import discord
-import json
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+import json
 from fifteen_api import FifteenAPI
 
 fDB = open('walterDB.json', encoding='utf-8')
@@ -18,11 +19,6 @@ intents.members = True
 
 client = commands.Bot(command_prefix=".", intents=intents)
 tts_api = FifteenAPI(show_debug=True)
-
-# response = tts_api.save_to_file("Fluttershy", "One more test", "tts.wav")
-# assert response["status"] == "OK"
-# assert response["filename"] == "tts.wav"
-# print(response)
 
 
 @client.event
@@ -64,12 +60,38 @@ async def on_message(message):
 
 
 @client.command(pass_context=True)
-async def join(ctx):
-    if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else:
+async def say(ctx, arg1, arg2):
+    user = ctx.message.author
+    channel = user.voice.channel
+
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    if (ctx.author.voice and voice == None):
+        voice = await channel.connect()
+    elif (not ctx.author.voice):
         await ctx.send("Cigane moraš biti u voicu!")
+        return
+
+    if(arg1 == "" or arg2 == ""):
+        ctx.send("Correct Syntax : .say <voice> <text>")
+        return
+
+    response = tts_api.save_to_file(arg1, arg2, "tts.wav")
+    assert response["status"] == "OK"
+    assert response["filename"] == "tts.wav"
+    print(response)
+
+    source = FFmpegPCMAudio("tts.wav")
+    player = voice.play(source)
+
+
+# @client.command(pass_context=True)
+# async def play(ctx):
+#     response = tts_api.save_to_file("Fluttershy", "One more test", "tts.wav")
+#     assert response["status"] == "OK"
+#     assert response["filename"] == "tts.wav"
+#     print(response)
+#     return
 
 
 @client.command(pass_context=True)
