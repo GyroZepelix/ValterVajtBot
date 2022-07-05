17c26862ad7680363e463be8d778a425182f3efa
Gyro <gamingskullgamer@gmail.com>
2022-07-05 23:36:30 +0200

    Added Voice Functionality

--- main.py
+++ main.py
@@ -5,18 +5,35 @@ from pydoc import cli
 from random import random, randrange
 import discord
 import json
+from discord.ext import commands
+from fifteen_api import FifteenAPI
 
 fDB = open('walterDB.json', encoding='utf-8')
 f = open('token.txt', "r")
 db = json.load(fDB)
 TOKEN = f.read()
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix=".", intents=intents)
+tts_api = FifteenAPI(show_debug=True)
+
+# response = tts_api.save_to_file("Fluttershy", "One more test", "tts.wav")
+# assert response["status"] == "OK"
+# assert response["filename"] == "tts.wav"
+# print(response)
+
+
+@client.event
+async def on_ready():
+    print(
+        "LOGIN SUCCESSFUL : {0.user}\n-----------------------------".format(client))
 
 
 @client.event
 async def on_message(message):
-    username = str(message.author).split('#')[0]
+    # username = str(message.author).split('#')[0]
     user_message = str(message.content)
     #channel = str(message.channel.name)
     #print(f'{username}: {user_message} ({channel})')
@@ -26,10 +43,15 @@ async def on_message(message):
 
     current_message = user_message.lower()
 
-    if current_message == "amogus":
-        await message.channel.send(f"Hello {username}!")
-        return
+    # --------------------------------------------------------------
 
+    # if current_message == ".join":
+    #     channel = client.get_channel('610548499192479748')
+    #     await client.join_voice_channel(channel)
+    #     print('Bot joined the channel.')
+    #     return
+
+    # --------------------------------------------------------------
     wordsMSG = current_message.split()
     keys = db["slavonijaSupremacyKeys"]
 
@@ -38,15 +60,31 @@ async def on_message(message):
             await message.channel.send(slavonijaSupremacy())
             return
 
+    await client.process_commands(message)
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("Cigane moraš biti u voicu!")
+
+
+@client.command(pass_context=True)
+async def fuckoff(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Aj ode ja crncu jedan!")
+    else:
+        await ctx.send("Alo ba! Ti si na kreku ako mislis da sam u voicechannelu!")
+
 
 def slavonijaSupremacy():
     lines = db["slavonijaSupremacy"]
     chosenMSG = randrange(0, len(lines))
     return lines[chosenMSG]
 
-
-@client.event
-async def on_ready():
-    print("LOGIN SUCCESSFUL : {0.user}".format(client))
 
 client.run(TOKEN)
